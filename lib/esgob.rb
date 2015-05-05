5d552a9ad741d6ca8c2e4cbca117d05e096ded25
Nicholas Humfrey <njh@aelius.com>
2015-05-05 18:24:53 +0100

    Added documentation comments to ServerError

--- lib/esgob.rb
+++ lib/esgob.rb
@@ -3,7 +3,9 @@ module Esgob
   autoload :CLI,      'esgob/cli'
   autoload :VERSION,  'esgob/version'
 
+  # Exception raised when the Esgob API returns an error
   class ServerError < StandardError
+    # Get the error code number returned by Esgob
     attr_reader :code
 
     def initialize(message, code = nil)
