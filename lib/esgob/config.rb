b9337235f69f143345b82721c7c70b43d9e83e36
Nicholas Humfrey <njh@aelius.com>
2015-05-15 08:31:10 +0100

    Made Esgob::Config a class and added a Esgob::Config#save method

--- lib/esgob/config.rb
+++ lib/esgob/config.rb
@@ -1,4 +1,8 @@
-module Esgob::Config
+class Esgob::Config
+  # @return [String]
+  attr_accessor :account
+  # @return [String]
+  attr_accessor :key
 
   # Get an ordered list of paths to possible Esgob configuration files
   def self.file_paths
@@ -7,5 +11,20 @@ module Esgob::Config
       '/etc/esgob',
       '/usr/local/etc/esgob'
     ]
+  end
+  
+  def file_paths
+    self.class.file_paths
+  end
+  
+  
+  def save(filepath=nil)
+    filepath = file_paths.first if filepath.nil?
+  
+    File.open(filepath, 'wb') do |file|
+      instance_variables.sort.each do |var|
+        file.puts "#{var.to_s.sub(/^@/,'')} #{instance_variable_get(var)}"
+      end
+    end
   end
 end
--- test/esgob_config_test.rb
+++ test/esgob_config_test.rb
@@ -17,4 +17,24 @@ class TestConfig < MiniTest::Unit::TestCase
     assert_includes(Esgob::Config.file_paths, '/etc/esgob')
   end
 
+  def test_instance_file_paths
+    conf = Esgob::Config.new
+    assert_instance_of(Array, conf.file_paths)
+    assert_includes(conf.file_paths, '/etc/esgob')
+  end
+
+  def test_save_config
+    tempfile = Tempfile.new('esgob-config-test')
+    tempfile.close
+
+    config = Esgob::Config.new
+    config.account = 'a'
+    config.key = 'k'
+    config.save(tempfile.path)
+
+    assert_equal("account a\nkey k\n", tempfile.open.read)
+
+    tempfile.unlink
+  end
+
 end
